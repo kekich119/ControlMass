33a66d0465210d5dbdf28759e1b3aaee6b906be2
kekich119 <patowach3o@gmail.com>
2023-09-16 18:39:52 +0300

    Первая функция

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,28 @@ public class Main {
     public static void main(String[] args) {
         int total = 0;
         Scanner in = new Scanner(System.in);
+        System.out.println("Что ты хочешь делать с массивами? ");
+        System.out.println("1. Сложить всё / 2. Перемножить / 3. Найти среднее арифметическое");
+        int answer = in.nextInt();
 
+        if (answer == 1){
+            plus();
+
+        }
+        if (answer == 2){
+
+        }
+        if (answer == 3){
+
+        }
+
+
+    }
+    public static void plus() {
         System.out.println("Напиши из скольки чисел будет массив: ");
+        Scanner in = new Scanner(System.in);
         int lenght = in.nextInt();
+        int total = 0;
         int NumF = 0;
         int numS = 0;
         int score = 0;
