ccb5dc89263ced4c549168fc50d0680347f7e300
kekich119 <patowach3o@gmail.com>
2023-09-16 18:29:01 +0300

    Это только начало...

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            int total = 0;
-
-
-
-
-
-            Scanner in = new Scanner(System.in);
-            System.out.print("Enter first number: ");
-            int NumF = in.nextInt();
-            System.out.println("Enter second numbers");
-            int numS = in.nextInt();
-
-            int[] array = {NumF, numS};
-            for (int i = 0; i < array.length; i++) {
-
-                total = total + array[i];
-
-                System.out.print(array[i] + " ");
-
-
-            }
-
-                System.out.println("\nСумма массива: " + total);
-                System.out.println("Напиши 1, что бы переключиться на новую программу");
-                int yesU = in.nextInt();
-
-                if (yesU == 1){
-
-                }
-
-
-
-    }
-    public  static void yes(){
+        int total = 0;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int NumF = in.nextInt();
-        System.out.println("Enter second numbers");
-        int numS = in.nextInt();
+
+        System.out.println("Напиши из скольки чисел будет массив: ");
+        int lenght = in.nextInt();
+        int NumF = 0;
+        int numS = 0;
+        int score = 0;
+        int array[] = new int[lenght];
+        System.out.println("Напиши числа для массива");
+
+        for (int i = 0; i < lenght; i++) {
+
+            score = i;
+            System.out.print(score + ": ");
+            array[i] = in.nextInt();
+
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+
+            total = total + array[i];
+
+            System.out.print(array[i] + " ");
+
+
+        }
+
+        System.out.println("\nСумма массива: " + total);
     }
 
 }
